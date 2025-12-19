726c2e83ce90c82eeb7187f3d46055c592ae1d87
dempz <69284007+dempz@users.noreply.github.com>
2025-12-19 13:37:12 +1100

    Change parameter name

--- R/thekids_save.R
+++ R/thekids_save.R
@@ -5,7 +5,7 @@
 #' @param filename File name to create on disk.
 #' @param path Path of the directory to save plot to.
 #' @param plot Plot to save; defaults to last plot displayed.
-#' @param output Size to save plot to. Can be a vector of strings. Default "full portrait".
+#' @param layout Size to save plot to. Can be a vector of strings. Default "full portrait".
 #' @param device Device to use. Can be a vector of strings. Default "pdf".
 #' @param dpi Plot resolution. Default 300.
 #' @param ... Other defaults passed to ggsave.
@@ -16,11 +16,11 @@
 #' geom_point() +
 #' theme_thekids()
 #'
-#' thekids_save("example", output = "half landscape", device = "png")
-#' thekids_save("example", output = "half portrait", device = "png")
-#' thekids_save("example", output = "full landscape", device = "png")
+#' thekids_save("example", layout = "half landscape", device = "png")
+#' thekids_save("example", layout = "half portrait", device = "png")
+#' thekids_save("example", layout = "full landscape", device = "png")
 #'
-#' thekids_save("example", output = c("half landscape", "half portrait"))
+#' thekids_save("example", layout = c("half landscape", "half portrait"))
 #' thekids_save("example", device = c("png", "pdf"))
 #' }
 #'
@@ -29,13 +29,13 @@
 thekids_save <- function(plot = ggplot2::last_plot(),
                          filename,
                          path = "output",
-                         output = "full portrait",
+                         layout = "full portrait",
                          device = "pdf",
                          dpi = 300,
                          ...) {
 
-    if (!all(output %in% unique(save_params$size))) {
-      stop(paste0("Your selection for 'output' must be from the list: ",
+    if (!all(layout %in% unique(save_params$size))) {
+      stop(paste0("Your selection for 'layout' must be from the list: ",
                   paste0("'", unique(save_params$size), "'",
                          collapse = ", "),
                   "."))
@@ -50,7 +50,7 @@ thekids_save <- function(plot = ggplot2::last_plot(),
 
     dir.create(path, showWarnings = FALSE, recursive = TRUE)
 
-    sel <- save_params[save_params$size %in% output & save_params$device %in% device, ]
+    sel <- save_params[save_params$size %in% layout & save_params$device %in% device, ]
 
     for (i in seq_len(nrow(sel))) {
       args <- sel[i, ]
