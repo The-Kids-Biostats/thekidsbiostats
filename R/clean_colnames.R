5e06323fb6b02b660dd20abdae2d82c4e4d2fc1c
Robin Cook <RobinCook9012@gmail.com>
2026-02-05 11:22:16 +0800

    Amended Column Cleaning functions and vignette
    Implemented most of the suggested changes to make_column_dict() and the cleaning_columns.Rmd suggest by @dempz.

--- R/clean_colnames.R
+++ R/clean_colnames.R
@@ -106,19 +106,33 @@ update_columns <- function(data, dict, old=NULL, new=NULL, label=NULL, reorder=F
 #' Create a copy-pasteable variable dictionary template
 #'
 #' @param data A data.frame or tibble containing the original data with column names to be replaced.
-#' @param auto_clean Logical, default TRUE: generate suggested new names using janitor::clean_names()
-#' @param file Optional character string. If specified, writes a CSV file with the template
+#' @param file Optional character string. If specified, writes dictionary template to file path (available formats: .csv, .txt, .xlsx, .rds)
 #' @param quiet Logical, default FALSE: suppress printing the generated R code string to the console.
+#' @param auto_clean Logical, default TRUE: generate suggested new names using \code{janitor::clean_names()}
+#' @param ... Additional arguments passed to \code{janitor::clean_names()}
 #'
-#' @return A character string containing a ready-to-paste tribble dictionary template
+#' @return
+#' A data.frame (invisibly returned) with one row per column name in \code{data} and three columns:
+#' \describe{
+#'   \item{old}{Original column names.}
+#'   \item{new}{Suggested new column names (optionally cleaned using
+#'     \code{janitor::clean_names()}).}
+#'   \item{label}{Empty character field for user-supplied variable labels.}
+#' }
+#'
+#' @details
+#' The default behaviour is to print a tribble template to the console. Set \code{quiet=TRUE} to suppress this output.
 #' @export
-make_column_dict <- function(data, auto_clean=TRUE, file=NULL, quiet=FALSE) {
+make_column_dict <- function(data, auto_clean=TRUE, file=NULL, quiet=FALSE, ...) {
 
   # Check if the data is dataframe-like
   if (!is.data.frame(data)) {
     stop("`data` must be a data.frame-like object", call. = FALSE)
   }
-  suffix <- gsub("^(df|dt|dat|data|tbl|tab)[_.]?", "", make.names(substitute(data)))  # useful for auto-naming the output, but probably a bit presumptuous
+  suffix <- make.names(substitute(data)) |> # useful for auto-naming the output
+    gsub("^(df|dt|dat|data|tbl|tab)[_.]?", "", x = _) |>
+    gsub("[_.]?(df|dt|dat|data|tbl|tab)$", "", x = _)
+
   suffix <- if (suffix == '') make.names(substitute(data)) else suffix
   # Suggested new names
   old_names <- names(data)
@@ -126,7 +140,7 @@ make_column_dict <- function(data, auto_clean=TRUE, file=NULL, quiet=FALSE) {
     if (!requireNamespace("janitor", quietly = TRUE)) {
       stop("Package 'janitor' to automatically rename columns.", call. = FALSE)
     }
-    new_names <- names(janitor::clean_names(data))
+    new_names <- names(janitor::clean_names(data, ...))
   } else {
     new_names = rep("", length(old_names))
   }
